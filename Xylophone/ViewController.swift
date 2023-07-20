6cc51f6a1b6efe7ea3cd1f4c094b5a98b9604cb2
RamonSynoro <ramon.fossa@triibo.com.br>
2023-07-20 15:22:22 -0300

    feature: Xylophone app finished

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        guard let buttonText = sender.titleLabel?.text else { return }
+        playSound(soundURL: buttonText)
+    }
+    
+    func playSound(soundURL: String) {
+        let url = Bundle.main.url(forResource: soundURL, withExtension: "wav")
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
